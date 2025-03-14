617366d245654b7aac7e23549600e1a596b306fb
Alec Powell <alecpowell@Mac.hsd1.ma.comcast.net>
2025-03-14 14:14:44 -0400

    removed the trailing / in corsOptions origin

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ const getImages = require("./Utility/getImages");
 
 const app = express();
 app.use(express.json());
+
 const corsOptions = {
-  origin: "https://stone-monkey1.github.io/sokal-qa-application/",
+  origin: "https://stone-monkey1.github.io/sokal-qa-application",
   methods: "GET,POST,OPTIONS",
   allowedHeaders: "Content-Type",
 };
